ebdc42b7c34f3cc22a61aa8f5187c484b74d599f
Xiuli Wei <weixiuliysu@gmail.com>
2025-09-15 11:08:45 +0000

    Address commnets

--- bolt/connectors/hive/storage_adapters/s3fs/S3Config.h
+++ bolt/connectors/hive/storage_adapters/s3fs/S3Config.h
@@ -91,7 +91,7 @@ class S3Config {
     kRetryMode,
     kUseProxyFromEnv,
     kCredentialsProvider,
-    KUploadPartAsync,
+    KPartUploadAsync,
     kPartUploadSize,
     KMaxConcurrentUploadNum,
     KUploadThreads,
@@ -133,8 +133,8 @@ class S3Config {
              std::make_pair("use-proxy-from-env", "false")},
             {Keys::kCredentialsProvider,
              std::make_pair("aws-credentials-provider", std::nullopt)},
-            {Keys::KUploadPartAsync,
-             std::make_pair("upload-part-async", "false")},
+            {Keys::KPartUploadAsync,
+             std::make_pair("part-upload-async", "false")},
             {Keys::kPartUploadSize,
              std::make_pair("part-upload-size", "10485760")},
             {Keys::KMaxConcurrentUploadNum,
@@ -269,8 +269,8 @@ class S3Config {
     return config_.find(Keys::kCredentialsProvider)->second;
   }
 
-  bool uploadPartAsync() const {
-    auto value = config_.find(Keys::KUploadPartAsync)->second.value();
+  bool partUploadAsync() const {
+    auto value = config_.find(Keys::KPartUploadAsync)->second.value();
     return folly::to<bool>(value);
   }
 
